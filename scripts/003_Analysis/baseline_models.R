bcd8138be765c1907df4cc6593b03d1be2dda8c2
nponarul <nponarul@gmail.com>
2021-10-12 14:40:35 -0400

    Added baseline metrics for two logistic regression models"

--- scripts/003_Analysis/baseline_models.R
+++ scripts/003_Analysis/baseline_models.R
@@ -15,11 +15,13 @@ library(tidyverse)
 library(ggplot2)
 library(corrplot)
 library(glmnet)
+library(caret)
 
 # LOAD DATA ---------------------------------------------------------------
 
 load('../../data/model_data_no_impute.rda')
 load('../../data/processed_data.rda')
+
 # PREPARE DATA ------------------------------------------------------------
 baseline_preds <- names(shhs1)[names(shhs1) %in% preds]
 # remove hosnr02
@@ -45,7 +47,14 @@ model_dat_filt <- model_data[, c('nsrrid', baseline_preds, outcome_vars) ] %>%
 sum(complete.cases(model_dat_filt))/nrow(model_dat_filt)
 # 0.6580722
 
+# complete cases data
+model_dat_filt_cc <- model_dat_filt[complete.cases(model_dat_filt), ]
 
+# TRAIN TEST SPLIT --------------------------------------------------------
+# Complete case
+train_ind <- sample(1:nrow(model_dat_filt_cc), size = floor(nrow(model_dat_filt_cc)*.8))
+test_ind <- 1:nrow(model_dat_filt_cc)
+test_ind <- test_ind[!test_ind %in% train_ind]
 # BASELINE MODELS ---------------------------------------------------------
 # Models that only have sleep architecture variables for predictors:
 # Sleep architecture – supinep, slpeffp, slpprdp, timeremp, times34p, timest1p, timest2p, waso
@@ -57,32 +66,48 @@ sleep_preds <- c("supinep", "slpeffp", "slpprdp", "timeremp",
 # Create model statements for four separate models (one with each outcome)
 model_form <- paste(outcome_vars, "~", paste0(sleep_preds, collapse = "+"))
 
-## A. Logistic regression (no regularization, complete cases)
-models_A <- list()
-for(i in seq_along(model_form)) {
-  models_A[[i]] <- glm(model_form[i], family = 'binomial', data = model_dat_filt[complete.cases(model_dat_filt), ])
+## A. Logistic regression (no regularization, complete cases, training set)
+
+get_model_metrics <- function(test_y, preds) {
+  precision <- posPredValue(preds, test_y)
+  recall <- sensitivity(preds, test_y)
+  F1 <- (2 * precision * recall) / (precision + recall)
+  print(paste("Positive Predictive Value: ", precision))
+  print(paste("Recall: ", recall))
+  print(paste("F1 Score: ", F1))
+  print(paste("Accuracy: ", mean(preds %>% as.numeric() == test_y %>% as.numeric())))
 }
 
-# Looking at the summaries of these models, predictors avgsat have strongest relationship with
-# any_cvd
-
-# Predictors slpprdp, waso, rdi3p, avgsat have strongest relationship with any_chd
-
+models_A <- list()
+pred_probs_A <- list()
+pred_class_A <- list()
+for(i in seq_along(model_form)) {
+  models_A[[i]] <- glm(model_form[i], family = 'binomial', data = model_dat_filt_cc[train_ind, ])
+  pred_probs_A[[i]] <- predict(models_A[[i]], model_dat_filt_cc[test_ind, ], type = "response")
+  pred_class_A[[i]] <- ifelse(pred_probs_A[[i]] > 0.5, 1, 0)
+  print(paste("Modeling: ", outcome_vars[i]))
+  get_model_metrics(as.factor(model_dat_filt_cc[test_ind, outcome_vars[i]] %>% unlist()), as.factor(pred_class_A[[i]]))
+  print(summary(models_A[[i]]))
+}
 
 ## B. Logistic regression (Lasso penalty)
 # glmmod <- glmnet(x, y=as.factor(asthma), alpha=1, family="binomial")
 models_B <- list()
+pred_probs_B <- list()
+pred_class_B <- list()
 for(i in seq_along(model_form)) {
-  X <- data.matrix(model_dat_filt[, sleep_preds][complete.cases(model_dat_filt[, sleep_preds]), ])
-  y <- data.matrix(model_dat_filt[, outcome_vars[i]][complete.cases(model_dat_filt[, sleep_preds]), ])
-  models_B[[i]] <- glmnet(X, y, family = 'binomial')
+  X_train <- data.matrix(model_dat_filt_cc[, sleep_preds][train_ind, ])
+  y_train <- data.matrix(model_dat_filt_cc[, outcome_vars[i]][train_ind, ])
+  X_test <- data.matrix(model_dat_filt_cc[, sleep_preds][test_ind, ])
+  y_test <- data.matrix(model_dat_filt_cc[, outcome_vars[i]][test_ind, ]) 
+  
+  models_B[[i]] <- glmnet(X_train, y_train, family = 'binomial')
+  pred_probs_B[[i]] <- predict(models_B[[i]], X_test, type = "response", s = min(models_B[[i]]$lambda))
+  pred_class_B[[i]] <- ifelse(pred_probs_B[[i]] > 0.5, 1, 0)
+  print(paste("Modeling: ", outcome_vars[i]))
+  get_model_metrics(as.factor(y_test), as.factor(pred_class_B[[i]]))
+  print(coef(models_B[[i]], s = min(models_B[[i]]$lambda)))
 }
-coef(models_B[[4]], s = min(models_B[[4]]$lambda))
-
-# any_cvd - removes times34p
-# any_chd - removes timeremp
-# cvd_death - removes times34p, rdi3p
-# chd_death - removes timeremp
 
 
 
