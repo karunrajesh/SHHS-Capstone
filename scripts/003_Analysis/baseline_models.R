671abffd0e63996e26b71b164dceae64dde6e12b
karunrajesh <karunrajesh@gmail.com>
2021-10-21 17:18:30 -0400

    Update baseline_models.R

--- scripts/003_Analysis/baseline_models.R
+++ scripts/003_Analysis/baseline_models.R
@@ -97,79 +97,87 @@ sum(complete.cases(model_dat_filt))/nrow(model_dat_filt)
 # complete cases data
 model_dat_filt_cc <- model_dat_filt[complete.cases(model_dat_filt), ]
 
-# TRAIN TEST SPLIT --------------------------------------------------------
-# Complete case
-train_ind <- sample(1:nrow(model_dat_filt_cc), size = floor(nrow(model_dat_filt_cc)*.8))
-test_ind <- 1:nrow(model_dat_filt_cc)
-test_ind <- test_ind[!test_ind %in% train_ind]
-# BASELINE MODELS ---------------------------------------------------------
-# Models that only have sleep architecture variables for predictors:
-# Sleep architecture – supinep, slpeffp, slpprdp, timeremp, times34p, timest1p, timest2p, waso
-# Polysomnography (osa) – rdi3p, ai_all, avgsat, minsat 
-sleep_preds <- c("supinep", "slpeffp", "slpprdp", "timeremp", 
-                 "times34p", "timest1p", "timest2p", "waso",
-                 "rdi3p", "ai_all", "avgsat", "minsat")
+# List of possible models
+models <- c(model_dat_filt_cc, trad_impute, mice_imputed)
 
-# Create model statements for four separate models (one with each outcome)
-model_form <- paste(outcome_vars, "~", paste0(sleep_preds, collapse = "+"))
-
-## A. Logistic regression (no regularization, complete cases, training set)
-
-get_model_metrics <- function(test_y, preds) {
-  precision <- posPredValue(preds, test_y)
-  recall <- sensitivity(preds, test_y)
-  F1 <- (2 * precision * recall) / (precision + recall)
-  print(paste("Positive Predictive Value: ", precision))
-  print(paste("Recall: ", recall))
-  print(paste("F1 Score: ", F1))
-  print(paste("Accuracy: ", mean(preds %>% as.numeric() == test_y %>% as.numeric())))
-}
-
-models_A <- list()
-pred_probs_A <- list()
-pred_class_A <- list()
-for(i in seq_along(model_form)) {
-  models_A[[i]] <- glm(model_form[i], family = 'binomial', data = model_dat_filt_cc[train_ind, ])
-  pred_probs_A[[i]] <- predict(models_A[[i]], model_dat_filt_cc[test_ind, ], type = "response")
-  pred_class_A[[i]] <- ifelse(pred_probs_A[[i]] > 0.5, 1, 0)
-  print(paste("Modeling: ", outcome_vars[i]))
-  get_model_metrics(as.factor(model_dat_filt_cc[test_ind, outcome_vars[i]] %>% unlist()), as.factor(pred_class_A[[i]]))
-  print(summary(models_A[[i]]))
-}
-
-## B. Logistic regression (Lasso penalty)
-# glmmod <- glmnet(x, y=as.factor(asthma), alpha=1, family="binomial")
-models_B <- list()
-pred_probs_B <- list()
-pred_class_B <- list()
-for(i in seq_along(model_form)) {
-  X_train <- data.matrix(model_dat_filt_cc[, sleep_preds][train_ind, ])
-  y_train <- data.matrix(model_dat_filt_cc[, outcome_vars[i]][train_ind, ])
-  X_test <- data.matrix(model_dat_filt_cc[, sleep_preds][test_ind, ])
-  y_test <- data.matrix(model_dat_filt_cc[, outcome_vars[i]][test_ind, ]) 
+for(model in models){
+  # TRAIN TEST SPLIT --------------------------------------------------------
+  # Complete case
+  model <- as.data.frame(model)
+  train_ind <- sample(1:nrow(model), size = floor(nrow(model)*.8))
+  test_ind <- 1:nrow(model)
+  test_ind <- test_ind[!test_ind %in% train_ind]
+  # BASELINE MODELS ---------------------------------------------------------
+  # Models that only have sleep architecture variables for predictors:
+  # Sleep architecture – supinep, slpeffp, slpprdp, timeremp, times34p, timest1p, timest2p, waso
+  # Polysomnography (osa) – rdi3p, ai_all, avgsat, minsat 
+  sleep_preds <- c("supinep", "slpeffp", "slpprdp", "timeremp", 
+                   "times34p", "timest1p", "timest2p", "waso",
+                   "rdi3p", "ai_all", "avgsat", "minsat")
   
-  models_B[[i]] <- glmnet(X_train, y_train, family = 'binomial')
-  pred_probs_B[[i]] <- predict(models_B[[i]], X_test, type = "response", s = min(models_B[[i]]$lambda))
-  pred_class_B[[i]] <- ifelse(pred_probs_B[[i]] > 0.5, 1, 0)
-  print(paste("Modeling: ", outcome_vars[i]))
-  get_model_metrics(as.factor(y_test), as.factor(pred_class_B[[i]]))
-  print(coef(models_B[[i]], s = min(models_B[[i]]$lambda)))
-}
-
-
-## C.  Logistic regression (no regularization, complete cases, training set, one covariate at a time)
-model_form_single <- expand.grid(outcome_vars, sleep_preds) %>% 
-                    mutate(model_form = paste(Var1,'~' ,Var2)) 
-
-models_C <- list()
-pred_probs_C <- list()
-pred_class_C <- list()
-for(i in 1:nrow(model_form_single)) {
-  print(paste("Modeling: ", model_form_single$model_form[i]))
-  models_C[[i]] <- glm(model_form_single$model_form[i], family = 'binomial', data = model_dat_filt_cc)
-  pred_probs_C[[i]] <- predict(models_C[[i]], model_dat_filt_cc, type = "response")
-  pred_class_C[[i]] <- ifelse(pred_probs_C[[i]] > 0.5, 1, 0)
+  # Create model statements for four separate models (one with each outcome)
+  model_form <- paste(outcome_vars, "~", paste0(sleep_preds, collapse = "+"))
   
-  get_model_metrics(as.factor(model_dat_filt_cc[, model_form_single$Var1[i]] %>% unlist()), as.factor(pred_class_C[[i]]))
-  print(summary(models_C[[i]]))
+  ## A. Logistic regression (no regularization, complete cases, training set)
+  
+  get_model_metrics <- function(test_y, preds) {
+    precision <- posPredValue(preds, test_y)
+    recall <- sensitivity(preds, test_y)
+    F1 <- (2 * precision * recall) / (precision + recall)
+    print(paste("Positive Predictive Value: ", precision))
+    print(paste("Recall: ", recall))
+    print(paste("F1 Score: ", F1))
+    print(paste("Accuracy: ", mean(preds %>% as.numeric() == test_y %>% as.numeric())))
+  }
+  
+  models_A <- list()
+  pred_probs_A <- list()
+  pred_class_A <- list()
+  for(i in seq_along(model_form)) {
+    models_A[[i]] <- glm(model_form[i], family = 'binomial', data = model[train_ind, ])
+    pred_probs_A[[i]] <- predict(models_A[[i]], model[test_ind, ], type = "response")
+    pred_class_A[[i]] <- ifelse(pred_probs_A[[i]] > 0.5, 1, 0)
+    print(paste("Modeling: ", outcome_vars[i]))
+    get_model_metrics(as.factor(model[test_ind, outcome_vars[i]] %>% unlist()), as.factor(pred_class_A[[i]]))
+    print(summary(models_A[[i]]))
+  }
+  
+  ## B. Logistic regression (Lasso penalty)
+  # glmmod <- glmnet(x, y=as.factor(asthma), alpha=1, family="binomial")
+  models_B <- list()
+  pred_probs_B <- list()
+  pred_class_B <- list()
+  for(i in seq_along(model_form)) {
+    X_train <- data.matrix(model[, sleep_preds][train_ind, ])
+    y_train <- data.matrix(model[, outcome_vars[i]][train_ind, ])
+    X_test <- data.matrix(model[, sleep_preds][test_ind, ])
+    y_test <- data.matrix(model[, outcome_vars[i]][test_ind, ]) 
+    
+    models_B[[i]] <- glmnet(X_train, y_train, family = 'binomial')
+    pred_probs_B[[i]] <- predict(models_B[[i]], X_test, type = "response", s = min(models_B[[i]]$lambda))
+    pred_class_B[[i]] <- ifelse(pred_probs_B[[i]] > 0.5, 1, 0)
+    print(paste("Modeling: ", outcome_vars[i]))
+    get_model_metrics(as.factor(y_test), as.factor(pred_class_B[[i]]))
+    print(coef(models_B[[i]], s = min(models_B[[i]]$lambda)))
+  }
+  
+  ## C.  Logistic regression (no regularization, complete cases, training set, one covariate at a time)
+  model_form_single <- expand.grid(outcome_vars, sleep_preds) %>% 
+    mutate(model_form = paste(Var1,'~' ,Var2)) 
+  
+  models_C <- list()
+  pred_probs_C <- list()
+  pred_class_C <- list()
+  for(i in 1:nrow(model_form_single)) {
+    print(paste("Modeling: ", model_form_single$model_form[i]))
+    models_C[[i]] <- glm(model_form_single$model_form[i], family = 'binomial', data = model_dat_filt_cc)
+    pred_probs_C[[i]] <- predict(models_C[[i]], model_dat_filt_cc, type = "response")
+    pred_class_C[[i]] <- ifelse(pred_probs_C[[i]] > 0.5, 1, 0)
+    
+    get_model_metrics(as.factor(model_dat_filt_cc[, model_form_single$Var1[i]] %>% unlist()), as.factor(pred_class_C[[i]]))
+    print(summary(models_C[[i]]))
+  }
+  
+  
+  
 }
